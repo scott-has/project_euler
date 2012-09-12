c26c94f69f4101f0f382857dae30346bb33fe931
Scott Hastie <shastie@fulcrum.net>
2012-09-12 16:22:05 -0700

    now gives correct answer

--- euler_002.rb
+++ euler_002.rb
@@ -1,9 +1,10 @@
 current = 1
-last = 1
+last = 0
 sum = 0
 until current >= 4000000 do 
-  sum += current if current.even? 
-  last = current
+  sum += current if current.even?
+  temp = current 
   current = current + last
+  last = temp
 end
 puts sum
